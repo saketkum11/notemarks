215dc4a83e7b6b9dea097bd19bcfcf584df8fcf0
saketkum11 <kumarsaket601@gmail.com>
2022-05-17 08:14:39 +0530

    added: route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.png";
 import { MyRoute } from "./MyRoute/MyRoute";
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <MyRoute />
+      <Outlet />
     </div>
   );
 }
